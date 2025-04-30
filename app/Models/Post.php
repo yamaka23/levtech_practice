f99894b7d260841977d5d11532ef1e2c05906872
SHOGO YAMAKAWA <showsan2727oneok@gmail.com>
2025-05-01 02:16:39 +0900

    ブログ投稿編集

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
     public function getByLimit(int $limit_count = 3)
     {
     // updated_atで降順に並べたあと、limitで件数制限をかける
@@ -19,8 +24,5 @@ class Post extends Model
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
-    protected $fillable = [
-        'title',
-        'body',
-    ];
+    
 }
