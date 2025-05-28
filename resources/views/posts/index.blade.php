a97e33f0370d766945b8d1961cf0bc1bf2eb9d44
SHOGO YAMAKAWA <showsan2727oneok@gmail.com>
2025-05-28 22:42:38 +0900

    API作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -85,6 +85,16 @@
             {{ $posts->links() }}    
         </div>
 
+        <div>
+            @foreach($questions as $question)
+                <div>
+                    <a href="https://teratail.com/questions/{{ $question['id'] }}">
+                        {{ $question['title'] }}
+                    </a>
+                </div>
+            @endforeach
+        </div>
+
         <script>
             function deletePost(id) {
                 'use strict';
